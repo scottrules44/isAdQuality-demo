2fd07cd2d1c85e4d56ce32f4d165a62ec46a3f07
Scott Harrison <scottrules44@gmail.com>
2022-09-24 21:47:44 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,35 @@
+local isAdQuality = require "plugin.isAdQuality"
+local widget =require "widget"
+local bg = display.newRect( display.contentCenterX, display.contentCenterY, display.actualContentWidth, display.actualContentHeight )
+bg:setFillColor( 0,0,1 )
+
+local title = display.newText( {text = "IS Quality Plugin", fontSize = 30} )
+title.width, title.height = 300, 168
+title.x, title.y = display.contentCenterX, 168*.5
+title:setFillColor(1,1,1)
+
+local params  = {
+    --required
+    iOSAppKey = "9865277d",
+    androidAppKey = "abbf956d",
+    --optional
+    logLevel = "error",
+    testMode = true,
+    userId = "1234",
+}
+
+
+isAdQuality.init( listener, params )
+isAdQuality.setUserConsent(true)
+
+local setUserId= widget.newButton({
+    label = "Set User Id",
+    x = display.contentCenterX,
+    y = display.contentCenterY-100,
+    id = "setUserId",
+    labelColor = { default={ 1, 1, 1 }, over={ 0, 0, 0, 0.5 } },
+    onRelease =  function ()
+        isAdQuality.setUserId("4321")
+    end
+
+})
